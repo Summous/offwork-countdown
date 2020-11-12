14d7904549b0301e3ab9bbc6c03b0fb97ffce7f4
X DY <xdy@XdeMac-mini.local>
2020-11-12 14:33:40 +0800

    feat：增加时间间隔

--- src/renderer/components/SetTimePage/SetTimePage.vue
+++ src/renderer/components/SetTimePage/SetTimePage.vue
@@ -17,34 +17,50 @@
         @click="handleClick"
         @change="handleChange"
       >
-        设定
+        {{timer ? '取消' : '设定'}}
       </el-button>
     </div>
-    <span v-if="showTips">请选择时间</span>
+    <div style="margin-top: 10px;" v-if="!!timer">
+        下次提醒时间 {{ showNextTime }}
+    </div>
+    <div v-if="showTips" style="font-size: 16px;margin-top: 10px;">{{showTips === 1 ? '请选择结束时间' : '下次提醒时间不能小于结束时间'}}</div>
   </div>
 </template>
 
 <script>
   import { ipcRenderer } from 'electron';
-  
+//   import dayjs from 'dayjs';
+
   export default {
     name: 'set-time-page',
     data() {
       return {
         time: null,
-        showTips: false,
+        showTips: 0,
         timer: null,
         input: null,
         nextTime: null,
       };
     },
+    computed: {
+      showNextTime() {
+        const setTime = new Date(this.nextTime * 1000);
+        return this.nextTime ? `${setTime.getHours()}:${setTime.getMinutes()}:${setTime.getSeconds()}` : `${new Date().getHours() + 1}:00:00`;
+      },
+    },
     methods: {
       handleClick() {
+        // 未设定结束时间
         if (!this.time) {
-          this.showTips = true;
+          this.showTips = 1;
           return;
         }
+        this.showTips = 0;
+        // 已计时，点击停止
         if (this.timer) {
+          clearInterval(this.timer);
+          this.timer = null;
+          this.nextTime = null;
           return;
         }
         const [hour, minute] = this.time.split(':');
@@ -52,25 +68,32 @@
         setTime.setHours(hour);
         setTime.setMinutes(minute);
         setTime.setSeconds(0);
-        // 判断当前时间是否为整点或设置的间隔时间, 以及是否以及到达time, 精确到秒
-        const checkTime = (time, isInput = true) => {
-          const now = new Date();
-          if (parseInt(now.getTime() / 1000, 10) === time) {
-            return {
-              reach: isInput,
-            };
-          }
-          return isInput && now.getMinutes() === 0 && now.getSeconds() === 0 ? {
-            reach: false,
-          } : null;
-        };
+        setTime = parseInt(setTime.getTime() / 1000, 10);
+        // 设定时间间隔
         if (this.input) {
           this.nextTime = parseInt(new Date().getTime() / 1000, 10) + (this.input * 60);
+          // 下次提醒时间超过结束时间
+          if (this.nextTime > setTime) {
+            this.showTips = 2;
+            return;
+          }
+        // 未设定时间间隔，结束时间小于当前时间
+        } else if (parseInt(new Date().getTime() / 1000, 10) > setTime) {
+          this.showTips = 2;
+          return;
         }
-        setTime = parseInt(setTime.getTime() / 1000, 10);
+        this.counter(setTime);
+      },
+      /**
+       * @description: 计时器
+       * @param {Date} setTime 结束时间
+       * @return {Null}
+       */
+      counter(setTime) {
         this.timer = setInterval(() => {
-          const check = this.input ? checkTime(this.nextTime < setTime ?
-            this.nextTime : setTime, this.nextTime > setTime) : checkTime(setTime);
+          console.log(parseInt(new Date().getTime() / 1000, 10), this.nextTime);
+          const check = this.input ? this.checkTime(this.nextTime < setTime ?
+            this.nextTime : setTime, this.nextTime > setTime) : this.checkTime(setTime);
           if (check) {
             if (this.input) {
               this.nextTime = this.nextTime + (this.input * 60);
@@ -84,10 +107,28 @@
           }
         }, 1000);
       },
-      handleChange() {
-        if (!this.showTips) {
-          this.showTips = true;
+      /**
+       * @description: 判断当前时间是否为整点或设置的间隔时间, 以及是否以及到达time, 精确到秒
+       * @param {Date} time 结束时间
+       * @param {Bollean} isInput 输入时间间隔
+       * @return {Object} 结束标志 { reach: true }、 { reach: false }、 null
+       */
+      checkTime(time, isInput = true) {
+        const now = new Date();
+        if (parseInt(now.getTime() / 1000, 10) === time) {
+          return {
+            reach: isInput,
+          };
         }
+        return isInput && now.getMinutes() === 0 && now.getSeconds() === 0 ? {
+          reach: false,
+        } : null;
+      },
+      handleChange() {
+        // console.log(1);
+        // if (!this.showTips) {
+        //   this.showTips = true;
+        // }
       },
     },
   };
